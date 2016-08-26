6b6f0d4e6c63260c3c26d210b2a59906410b8884
Travis Rich <travis.s.rich@gmail.com>
2016-08-26 18:58:19 -0400

    Add Begin Draft link

---
+++ src/BeginDraft/BeginDraft.js
@@ -0,0 +1,20 @@
+import React, {PropTypes} from 'react';
+
+export const About = React.createClass({
+	propTypes: {
+		routeParams: PropTypes.object,
+	},
+
+	render: function() {
+		return (
+			<div className="about-iframe-container">
+
+				<iframe className="article-iframe" src={'https://www.pubpub.org/pub/begin-draft?embed=true&hideRightPanel=true'}/>
+
+			</div>
+		);
+	}
+
+});
+
+export default About;
